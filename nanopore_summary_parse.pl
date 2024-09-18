c7cbc0c28fb0b6862c62865a8ca873d2e0448742
kosmoflips <kosmoflips@gmail.com>
2024-09-18 21:29:55 +0000

    calc N50

--- nanopore_summary_parse.pl
+++ nanopore_summary_parse.pl
@@ -4,7 +4,6 @@ use warnings;
 # parse and extract needed info from nanopore seq output file "sequencing_summary_XYZ.txt"
 # more info: https://community.nanoporetech.com/docs/prepare/library_prep_protocols/Guppy-protocol/v/gpb_2003_v1_revax_14dec2018/input-and-output-files#:~:text=sequencing_summary.,emitted%20containing%20the%20basecall%20results.
 
-
 use Data::Dumper;
 use File::Spec;
 use File::Path;
@@ -51,7 +50,11 @@ if (!-d $odir) {
 }
 
 open (my $fh2, ">", $ofile);
-printf $fh2 "%s\n", join "\t", qw/summary_file_path   passed_reads   failed_reads   passed_total_bases   failed_total_bases /;
+printf $fh2 "%s\n", join "\t", qw/
+	summary_file_path
+	pass_reads   pass_total_bases  pass_read_max  pass_read_min  pass_read_N50
+	fail_reads   fail_total_bases  fail_read_max  fail_read_min  fail_read_N50
+/;
 
 if ($filelist and -e $filelist) {
 	open (my $fh, $filelist);
@@ -63,11 +66,10 @@ if ($filelist and -e $filelist) {
 	close ($fh);
 }
 
-# my $x=0;
 foreach my $f0 (@files) {
 	printf "> %s . . .\n", $f0;
 	my $f;
-	if (-d $f0) { # input dir, need to look for seq-sum.txt under this dir
+	if (-d $f0) { # if input dir, need to look for seq-sum.txt under this dir
 		opendir (my $dh, $f0);
 		while (my $f1=readdir $dh) {
 			if ($f1=~/^sequencing_summary\S+\.txt$/i) {
@@ -75,12 +77,12 @@ foreach my $f0 (@files) {
 				last;
 			}
 		}
-	}
-	elsif (-e $f0) { # input file, will just assume it's a valid "sequencing_summary.txt" file
+	} else {
 		$f=$f0;
 	}
-	else {
-		print "    path doesn't exist, skip\n";
+
+	if (!$f or !-e $f) {
+		print "    can't find proper \"sequencing_summary\" file, skip\n";
 		printf $fh2 "%s\t-1\n", $f0;
 		next;
 	}
@@ -89,6 +91,7 @@ foreach my $f0 (@files) {
 		'pass'=>{total_read=>0, total_len=>0},
 		'fail'=>{total_read=>0, total_len=>0}
 	};
+	my $reads={};
 
 	my $idx_seqlen=-1;
 	my $idx_passfail=-1;
@@ -108,12 +111,41 @@ foreach my $f0 (@files) {
 			my $len=$c[$idx_seqlen]; # length of current fastq read in bases
 			$stat->{$passfail}{total_read}++;
 			$stat->{$passfail}{total_len}+=$len;
+			push @{$reads->{$passfail}}, $len;
+			# print Dumper $reads->{$passfail};<>;
 		}
 	}
 	close ($fh);
-	printf $fh2 "%s\n", join "\t", $f, $stat->{pass}{total_read}, $stat->{fail}{total_read}, $stat->{pass}{total_len}, $stat->{fail}{total_len};
-	# $x++;
-	# last if $x==3;
+	# calc N50
+	# https://timkahlke.github.io/LongRead_tutorials/APP_MET.html#:~:text=The%20N50%20is%20related%20to,in%20the%20set%20of%20sequences.
+	foreach my $pf ("pass", "fail") {
+		my $n50=$stat->{$pf}{total_len}/2;
+		my $reads_sorted = [ sort {$b<=>$a} @{$reads->{$pf}} ];
+		# die $reads_sorted[-1];
+		$stat->{$pf}{'read_max'} = $reads_sorted->[0];
+		for my $i (1..scalar(@$reads_sorted)) {
+			my $j=$i*-1;
+			if ($reads_sorted->[$j]>0) {
+				$stat->{$pf}{'read_min'} = $reads_sorted->[$j]; # exclude 0 from min read len
+				last;
+			}
+		}
+		my $lensum=0;
+		foreach my $r1 (@$reads_sorted) {
+			$lensum+=$r1;
+			if ($lensum>=$n50) {
+				$stat->{$pf}{'N50'} = $r1;
+				last;
+			}
+		}
+	}
+	# die Dumper $stat;
+
+	printf $fh2 "%s\n",
+		join "\t", ( $f,
+			$stat->{pass}{total_read}, $stat->{pass}{total_len}, $stat->{pass}{read_max}, $stat->{pass}{read_min}, $stat->{pass}{N50},
+			$stat->{fail}{total_read}, $stat->{fail}{total_len}, $stat->{fail}{read_max}, $stat->{fail}{read_min}, $stat->{fail}{N50}
+		);
 }
 
 print "\n\nall done, output stat data written to ", $ofile;
